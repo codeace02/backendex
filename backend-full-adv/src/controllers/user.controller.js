91d28ac51b43669a5de3fa0d6c5181f76b0fb240
hp <amisha9565@gmail.com>
2024-09-10 16:20:09 +0530

    user controller logic for getting current user

--- backend-full-adv/src/controllers/user.controller.js
+++ backend-full-adv/src/controllers/user.controller.js
@@ -260,4 +260,14 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
         )
 })
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+        .json(
+            200,
+            req.user,
+            "Current user fetched successfully!"
+        );
+})
+
 export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser }
