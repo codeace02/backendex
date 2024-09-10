4e6fde9abb245b1b69b2418274c45f1a1ff56571
hp <amisha9565@gmail.com>
2024-09-10 16:18:06 +0530

    user controller logic for change current user's password

--- backend-full-adv/src/controllers/user.controller.js
+++ backend-full-adv/src/controllers/user.controller.js
@@ -230,4 +230,34 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken }
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+
+    const { oldPassword, newPassword, cnfPassword } = req?.body;
+
+    if (!(newPassword === cnfPassword)) {
+        throw new ApiError(400, "New password & confirm password must be same!")
+    }
+
+    const user = await User.findById(req?.user?._id);
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password!");
+    }
+
+    user.password = newPassword;
+    await user.save({ validateBeforeSave: false });
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Password changed successfully!"
+            )
+        )
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser }
