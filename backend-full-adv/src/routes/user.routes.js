f8acd8ab2e0cfbba369897e37e2e313cec729876
Amisha <amisha9565@gmail.com>
2024-09-11 23:40:44 +0530

    route define for getting profile details of user

--- backend-full-adv/src/routes/user.routes.js
+++ backend-full-adv/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,5 +26,6 @@ router.route('/login').post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/get-profile").post(verifyJWT, getCurrentUser)
 
 export default router;
